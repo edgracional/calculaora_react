bc9bf46d29653320a7c3547623fb9b6ebe7be3c5
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-03 14:49:44 -0300

    48. Implementando a Lógica #01

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,19 @@ import './Calculator.css'
 import Button from "../components/Button";
 import Display from "../components/Display";
 
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+
+}
+
 export default class Calculator extends Component {
+
+    state = { ...initialState }
 
     constructor(props) {
         super(props)
@@ -14,15 +26,34 @@ export default class Calculator extends Component {
     }
 
 clearMemory() {
-    console.log('limpar')
+    this.setState({ ...initialState })
 }
 
 setOperation(operation) {
-    console.log(operation)
+    
 }
 
 addDigit(n) {
-    console.log(n)
+    if (n === '.' && this.state.displayValue.include('.')) {
+        return
+    }
+
+    const clearDisplay = this.state.displayValue === '0'
+        || this.state.clearDisplay
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+        const i = this.state.current
+        const newValue = parseFloat(displayValue)
+        const values = [...this.state.value]
+        values[i] = newValue
+        this.setState({ values })
+    }
+
+
 }
 
 
@@ -30,7 +61,7 @@ addDigit(n) {
 
         return (
             <div className="caculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.clearMemory} operation />
                 <Button label="7" click={this.setOperation} />
